ee799a34fe45ea29b45f14b8eb5685babbee95e1
Manuel Grundner <m.grundner@delegate.at>
2014-03-08 17:09:27 +0100

    added missing files

---
+++ XpandBlog.DTO/Security/User.cs
@@ -0,0 +1,18 @@
+﻿using XpandBlog.Contracts.Security;
+
+namespace XpandBlog.DTO.Security
+{
+    public class User : IUser, Microsoft.AspNet.Identity.IUser<int>
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public bool IsActive { get; set; }
+        public string PasswordHash { get; set; }
+        
+        string Microsoft.AspNet.Identity.IUser<int>.UserName
+        {
+            get { return Username; }
+            set { Username = value; }
+        }
+    }
+}
---
+++ XpandBlog.Model/Base/BlogBaseObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using DevExpress.Xpo;
+using XpandBlog.Model.Annotations;
+
+namespace XpandBlog.Model
+{
+    [NonPersistent]
+    public abstract class BlogBaseObject : XPBaseObject
+    {
+        protected BlogBaseObject(Session session) : base(session)
+        {
+        }
+
+        [NotifyPropertyChangedInvocator]
+        public bool SetPropertyValue<T>(ref T valueHolder, T value, [CallerMemberName] string propertyName = null)
+        {
+            return SetPropertyValue(propertyName, ref valueHolder, value);
+        }
+    }
+}
